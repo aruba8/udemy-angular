36948e72e3aebe32c7091b4516d19597085ae8a5
Erik Khalimov <biomaks@gmail.com>
2018-01-23 19:10:22 -0600

    work with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import {RecipesComponent} from './recipes/recipes.component';
 import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShopingEditComponent } from './shopping-list/shoping-edit/shoping-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
@@ -20,7 +20,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    ShopingEditComponent,
+    ShoppingEditComponent,
     ShoppingListComponent
   ],
   imports: [
